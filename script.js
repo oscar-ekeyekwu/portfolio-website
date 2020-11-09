f0e84fe489b04102676933c23d228334354ef6b0
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-11-09 05:20:33 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var dropdownBtn = document.getElementById("dropdown-btn");
+var logo = document.getElementById("logo");
+var menu = document.getElementById("menu");
+var icon = document.getElementById("icon");
+
+// for sticky header
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+dropdownBtn.addEventListener("click", function (e) {
+  if (menu.className === "links-cont" && logo.className === "logo-cont") {
+    menu.className += " responsive";
+    logo.className += " responsive";
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
+  } else {
+    menu.className = "links-cont";
+    logo.className = "logo-cont";
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    menu.classList.remove("responsive");
+    logo.classList.remove("responsive");
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
